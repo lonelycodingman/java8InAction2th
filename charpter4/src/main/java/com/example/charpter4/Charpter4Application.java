f9e332356ffcaa9bae3954a6569c8461c8ff76b6
lonelycodingman <1367425226@qq.com>
2021-06-27 11:16:44 +0800

    解决pom.xml报红的问题

---
+++ charpter4/src/main/java/com/example/charpter4/Charpter4Application.java
@@ -0,0 +1,13 @@
+package com.example.charpter4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Charpter4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Charpter4Application.class, args);
+    }
+
+}
